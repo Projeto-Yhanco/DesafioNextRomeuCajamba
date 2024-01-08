15ea283bffe835caf8a50dc86efdbcfd58c0f096
RomeuCajamba <romeucajamba@gmail.com>
2024-01-08 14:10:35 +0100

    montar toda lógica

---
+++ desafio/src/app/layout/main/data.ts
@@ -0,0 +1,54 @@
+import Osvaldo from '../../../assets/Osvaldo.svg';
+import Romeu from '../../../assets/Romeu.svg';
+import Nicolau from '../../../assets/Milvaneo.svg';
+import Lourenco from '../../p';
+
+
+
+export type User = {
+    name: string;
+    task: string;
+    img: string;
+};
+
+export type dataType = {
+    Users: User[],
+    about: string,
+    video: string
+}
+
+
+export const dataYhanko: dataType = {
+    Users: [
+        {
+            name: 'Osvaldo Pedro',
+            task: 'Backend Developer',
+            img:  Osvaldo
+        },
+        {
+            name: 'Romeu Cajamba',
+            task: 'Backend Developer',
+            img: Romeu
+        },
+        {
+            name: 'Nicolau Noberto',
+            task: 'Marketing',
+            img:  Nicolau
+        },
+        {
+            name: 'Lorrys Code',
+            task: 'Fron-End Developer',
+            img:  Lourenco
+        }
+    ],
+    about: `
+    A Yhanko e uma que tem como objetivo criação de projetos
+    com ferramentas modernas, trazendo uma melhor experine
+    cia pora o usuario, a Yhanko tem em sua constituição dev’s 
+    com habilidades unicas e inovadoras, o seu papel na aréa e
+    comercial  é de expandir, inovar e pontencializar qualquer 
+    outro projeto, do pensamento para o template ate ao codigo
+    ate ao codigp, Yhanko é o fututo.
+    `,
+    video: '../../public/video.webm'
+}
--- desafio/src/app/layout/main/main.tsx
+++ desafio/src/app/layout/main/main.tsx
@@ -1,27 +1,125 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import {UserContainer} from './user/index';
-import {ContainerAbout} from './about/index';
-import {History} from './history/index';
+"use client";
+
+import { useReducer, useState } from "react";
+import { User, dataYhanko } from "./data";
+
+type Dados = {
+  dataUsers: string;
+  usersData: User[]
+};
+
+type ActionResult = "USERS" | "ABOUT" | "VIDEO";
+
+const result = (state: Dados, action: ActionResult): Dados => {
+  switch (action) {
+    case "USERS":
+      return { ...state, usersData: dataYhanko.Users};
+    case "ABOUT":
+      return { ...state, dataUsers: dataYhanko.about };
+    case "VIDEO":
+      return { ...state, dataUsers: dataYhanko.video };
+  }
+};
 
 export function Main(){
+    const initial: Dados = { dataUsers: "", usersData: []};
+    const [state, dispach] = useReducer(result, initial);
+    const [mouse, setMouse] = useState(false);
+    const [controlState, setControLState] = useState("");
+    const mouseActive = (action: ActionResult) => {
+      dispach(`${action}`);
+      setControLState(action);
+      setMouse(true);
+    };
+  
+    const mouseInative = () => {
+      setMouse(false);
+    };
+
+
+
+
     
     return(
         <main className="h-[400px]">
-            <ul className=" mt-[30px] ml-[168px] flex flex-row space-x-4">
+             <section className="w-[800px] h-[400px] ">
+        <div className="flex gap-4 mb-[50px]">
+          <button
+            onMouseEnter={() => mouseActive("USERS")}
+            onMouseLeave={mouseInative}
+            className="px-[2rem] py-[0.5rem] rounded-sm bg-[#F76680] text-slate-100 font-semibold
+         text-[14px] hover:bg-[#FF7EB6]"
+          >
+            Users
+          </button>
 
-                <li className="w-[145px] h-[45px] rounded-[7px] bg-bcol text-center text-white text-[18px] font-semibold pt-2  hover:bg-btclick hover:cursor-pointer">
-                    <Link href="/[slug]" as="/user">User</Link>
-                </li>
+          <button
+            onMouseEnter={() => mouseActive("ABOUT")}
+            onMouseLeave={mouseInative}
+            className="px-[2rem] py-[0.5rem] rounded-sm bg-[#F76680] text-slate-100 font-semibold
+         text-[14px] hover:bg-[#FF7EB6]"
+          >
+            About Yhanko
+          </button>
 
-                <li className="w-[145px] h-[45px] rounded-[7px] bg-bcol text-center  text-white text-[18px] font-semibold pt-2  hover:bg-btclick hover:cursor-pointer">
-                    <Link href="/about" as="/about">About Yhanko</Link>
-                </li>
+          <button
+            onMouseEnter={() => mouseActive("VIDEO")}
+            onMouseLeave={mouseInative}
+            className="px-[2rem] py-[0.5rem] rounded-sm bg-[#F76680] text-slate-100 font-semibold
+         text-[14px] hover:bg-[#FF7EB6]"
+          >
+            History
+          </button>
+        </div>
 
-                <li className="w-[145px] h-[45px] rounded-[7px] bg-bcol text-center text-white text-[18px] font-semibold pt-2  hover:bg-btclick hover:cursor-pointer">
-                    <Link href="/[slug]" as="/history">History</Link>
-                 </li>
-            </ul>
+        <div id="body">
+          {controlState === "USERS" ? (
+            <>
+              <div>
+                 {
+                  state.usersData.map((user, key) => (
+                    <div key={key} className="flex gap-5 mb-5">
+                  <Image src={user.img} alt="foto perfil" width={50} height={50} />
+                  <div className="flex flex-col gap-1">
+                    <h2 className="text-[16px] text-slate-200">
+                      {user.name}
+                    </h2>
+                    <p className="text-[12px] text-zinc-400">
+                      {user.task}
+                    </p>
+                  </div>
+                </div>
+                  ))
+                 }
+              </div>
+            </>
+          ) : controlState === "ABOUT" ? (
+            <>
+              <p className="text-[20px] text-slate-100">{state.dataUsers}</p>
+            </>
+          ) : controlState === "VIDEO" ? (
+            <>
+            <div className="w-[900px] h-[300px] border-4 border-transparent rounded-lg overflow-hidden">
+            <video
+                src="/videoNew.mp4"
+                className="object-cover"
+                controls
+                autoPlay
+                loop
+                muted
+              >
+                Your browser does not support the video tag.
+              </video>
+            </div>
+             
+            </>
+          ) : (
+            <>
+            <p>Carregando</p>
+            </>
+          )}
+        </div>
+      </section>
         </main>
     )
 }
